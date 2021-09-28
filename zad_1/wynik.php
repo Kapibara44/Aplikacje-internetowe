e12e969526d9889ef9a28d71bb86f021086f91b9
Kapibara44 <kacper.kachel44@wp.pl>
2021-09-28 11:12:54 +0200

    Zadanie_1

---
+++ zad_1/wynik.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Testowanie skryptów PHP</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <?php
+        $a=$_POST['pole_1'];
+
+        echo "Witaj "; echo $a; echo " na naszej stronie"; 
+    ?>
+  </body>
+</html>
